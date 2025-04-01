17a18af125df07f5425e405afb3c4bcf65b7f7d6
nickkats1 <katsarelas1@example.com>
2025-04-01 13:07:11 -0400

    first commit

---
+++ houseprice/app/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 07:04:55 2025
+
+@author: katsa
+"""
+
---
+++ houseprice/app/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  1 07:05:13 2025
+
+@author: katsa
+"""
+
