fc8d0aea21134e9f21b18dc180725fa1d32df217
nickkats1 <katsarelasnick3@gmail.com>
2025-10-10 17:02:49 -0400

    minor

--- creditrisk/src/data_aquisition/data_ingestion.py
+++ creditrisk/src/data_aquisition/data_ingestion.py
@@ -1,30 +1,63 @@
-from helpers.config import load_config
-from helpers.logger import logger
 import pandas as pd
+import numpy as np
+from helpers.config import load_config
 from urllib.request import urlretrieve
+import numpy as np
+from helpers import logger
+from sklearn.model_selection import train_test_split
+
+
+
 
 class DataIngestion:
     def __init__(self,config):
-        self.config=  config
+        self.config = config
         
+        
+    
     def fetch_data(self):
-        """" Fetch data from url """
+        """Fetch data from url link"""
         try:
-            # path to url
-            URL_PATH = self.config['url_link']
-            # path to raw path
+            URL_LINK = self.config['url_link']
             RAW_PATH = self.config['raw_path']
-            urlretrieve(url=URL_PATH,filename=RAW_PATH)
-            
-            
-        except Exception as e:
-            logger.exception(f"Could not find path: {e}")
-            raise e
         
+            urlretrieve(url=URL_LINK,filename=RAW_PATH)
+            return URL_LINK,RAW_PATH
+        except Exception as e:
+            logger.error(f"Url could not be loaded to path: {e}")
+            raise e
+    
+    def load_data(self) -> pd.DataFrame:
+        """ converts urlretrive to dataframe """
+        try:
+            data = pd.read_csv(self.config['raw_path'],delimiter=",")
+            data.drop_duplicates(inplace=True)
+            data.to_csv(self.config['raw_path'],index=0)
+            
+            #features to be split
+            features = data.drop('Default',axis=1)
+            target = data['Default']
+            
+            self.df_train,self.df_test = train_test_split(features,test_size=.20,random_state=42)
+            self.df_train.to_csv(self.config['train_raw'],index=0)
+            self.df_test.to_csv(self.config['test_raw'],index=0)
+            
+            # target features split
+            self.y_train_df,self.y_test_df = train_test_split(target,test_size=0.20,random_state=42)
+            self.y_train_df.to_csv(self.config['train_target_raw'],index=0)
+            self.y_test_df.to_csv(self.config['test_target_raw'],index=0)
+            
+            
 
+        except FileExistsError as e:
+            logger.exception(f"Could not find file: {e}")
+            raise
+
+        
+        
 
 if __name__ == "__main__":
     config = load_config()
-    data_ingestion_config = DataIngestion(config)
+    data_ingestion_config=  DataIngestion(config)
     data_ingestion_config.fetch_data()
-
+    data_ingestion_config.load_data()
--- creditrisk/src/data_aquisition/data_transformation.py
+++ creditrisk/src/data_aquisition/data_transformation.py
@@ -1,69 +1,35 @@
 from helpers.config import load_config
 from helpers.logger import logger
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder,StandardScaler
-from sklearn.compose import make_column_transformer
-from sklearn.pipeline import make_pipeline
-from sklearn.model_selection import train_test_split
+
 
 class DataTransformation:
     def __init__(self,config):
         self.config = config
         
-        
-    def data_cleaning(self):
-        """ Clean data """
+    def transform_data(self):
+        """ fetch data from DataIngestion and scaled the train test split"""
         try:
-            self.df = pd.read_csv(self.config['raw_path'],delimiter=",")
-            # features
-            self.features=  self.config['features']
-            self.training_features = self.df.drop('Default',axis=1)
+            df_train = pd.read_csv(self.config['train_raw'],delimiter=",")
+            df_test = pd.read_csv(self.config['test_raw'],delimiter=",")
             
-            
-            # column transformer
-            self.ct = make_column_transformer(
-                (OneHotEncoder(),self.training_features.select_dtypes(include="object").columns),
-                (StandardScaler(),self.training_features.select_dtypes(include="number").columns)
-                ,remainder="passthrough")
-            
-            self.df_transformed = self.ct.fit_transform(self.training_features)
-            self.df_transformed = pd.DataFrame(self.training_features)
-            self.df_transformed.to_csv(self.config['processed_path'])
-            return self.df_transformed
-        
+            # scaling data
+            scaler = StandardScaler()
+            df_train_scaled = scaler.fit_transform(df_train)
+            df_test_scaled = scaler.transform(df_test)
+            # turning arrays to dataframe
+            df_train_scaled = pd.DataFrame(df_train_scaled)
+            df_test_scaled = pd.DataFrame(df_test_scaled)
+            df_train_scaled.to_csv(self.config['train_processed'],index=0)
+            df_test_scaled.to_csv(self.config['test_processed'],index=0)
         except Exception as e:
-            logger.exception(f"failed: {e}")
-            raise e
+            logger.exception(f"Could not transform data")
+            raise
         
-    def split(self):
-        """ Split features """
-        try:
-            # training and testing split
-            self.df_train,self.df_test = train_test_split(self.features,test_size=0.10,random_state=1)
-            logger.info(f'Length of df_train: {len(self.df_train)}')
-            logger.info(f'Length of df_test: {len(self.df_test)}')
-            # df_train and df_test to .csv
-            self.df_train.to_csv(self.config['train_processed'])
-            self.df_test.to_csv(self.config['test_processed'])
-            
-            # split target for training and testing data
-            target = self.df['Default']
-            
-            self.y_train_df,self.y_test_df = train_test_split(target,test_size=.10,random_state=1)
-            logger.info(f'Length of y_train_df: {len(self.y_train_df)}')
-            logger.info(f'Length of y_test_df: {len(self.y_test_df)}')
-            
-            #y_train and y_test to dataframe
-            self.y_train_df.to_csv(self.config['train_target_raw'],index=0)
-            self.y_test_df.to_csv(self.config['test_target_raw'],index=0)
-            return self.y_train_df,self.y_test_df
-        except Exception as e:
-            raise e
-
 
 
 if __name__ == "__main__":
     config = load_config()
     data_transformation_config = DataTransformation(config)
-    data_transformation_config.data_cleaning()
-    data_transformation_config.split()
+    data_transformation_config.transform_data()
